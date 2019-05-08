d9e7f7f792d269d1d47d7ecfb7142a46ef188100
Diego Burlando <you@example.com>
2019-05-08 16:38:46 +0200

    ciao

--- nginxconfiguration/Read.md
+++
@@ -1,3 +0,0 @@
-Just install nginx on the host machine and then use the default file in this folder to configure it as a load balancer.
-
-the file must be placed in ...../nginx/sites-available  folder.
--- truncatesqllite.sql
+++
@@ -1,8 +0,0 @@
-﻿-- these command must be issued to reset the primary keys in sqllite tables
-
-
-delete from jwtsecrets;    
-delete from sqlite_sequence where name='jwtsecrets';
-
-delete from  users;    
-delete from sqlite_sequence where name='users';
