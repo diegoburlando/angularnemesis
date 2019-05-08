c57e1a56ad5de659c4715294fdb0c0b3bdfd7f65
Diego Burlando <you@example.com>
2019-05-08 14:52:37 +0200

    ciao

--- verifyuser.js
+++ verifyuser.js
@@ -4,15 +4,25 @@ var verifyuser = function(userid) {
     return new Promise((resolve,reject)=> { 
         var db = new sqlite3.Database(__dirname + '/../sql/marydb.db'); 
         db.serialize(() => {
-            db.run(`UPDATE users SET userIsVerified = '1' WHERE userId = '${userid}'`, function(err)  {
+            db.run(`UPDATE users SET userIsVerified = '1' WHERE userId = '${userid}'`, function(errUpdate)  {
                                            
-                if (err) {
-                    db.close((err) => {if (err) return console.error(err.message); console.log('Close the database connection.', db);});
-                    return reject({success:false, message:err.message});
+                if (errUpdate) {
+                    db.close((errClose) => {
+                        if (errClose) {
+                            console.error(errClose.message); 
+                            return;
+                        }
+                    });
+                    return reject({success:false, message:errUpdate.message});
                 }
-                db.close((err) => {if (err) return console.error(err.message); console.log('Close the database connection.', db);});
+                db.close((errClose1) => {
+                    if (errClose1) {
+                        console.error(errClose1.message); 
+                        return;
+                    }
+                });
                 if(this.changes === 1) return resolve({success:true});
-                return reject({success:false, message:err.message});
+                return reject({success:false, message:errClose1.message});
 
             });
         });
