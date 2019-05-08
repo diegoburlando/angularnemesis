36b00b88a3bae86a6bc3915c84493d064c5a33b4
Diego <burlandodiego@alice.it>
2019-05-08 12:01:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = require('http').Server(app)
 
 app.get('/',   (req, res) => { res.send("Hello Everybody")});
 
-const listener = server.listen(process.env.PORT || 8000, () => { 
+const listener = server.listen(process.env.PORT || 3000, () => { 
     
     
     console.log(`Application worker ${process.pid} started... on port ${listener.address().port} `);
